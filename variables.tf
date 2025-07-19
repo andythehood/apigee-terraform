9dc0369fbd46b1e8ec216c751716e24fdd9be1ae
Andy Hood <andythehood@gmail.com>
2025-07-19 16:26:02 +1000

    update dependencies

--- variables.tf
+++ variables.tf
@@ -2,14 +2,14 @@ variable "apigee_project_id" {
   description = "The ID of the Apigee project"
   type        = string
   default     = "aviato-andy-apigee-tf-fk"
-  # default     = "prj-wrk-eiip-apigw-org-dev-zj" # Uncomment if using a different project ID
+  # default     = "prj-wrk-eiip-apigw-org-dev-zj" 
 }
 
 variable "hub_project_id" {
   description = "The ID of the Hub Host project"
   type        = string
   default     = "aviato-andy-apigee-tf-fk"
-  # default     = "prj-wrk-eiip-apigw-org-dev-zj" # Uncomment if using a different project ID
+  # default     = "prj-core-net-hub-d4s3" 
 }
 
 variable "region" {
